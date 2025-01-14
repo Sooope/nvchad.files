a788f3502cc040ad160df251b0bf31eca32dbd9b
Sooope <kindraddish@gmail.com>
2025-01-14 16:46:04 +0800

    init

---
+++ lua/mappings.lua
@@ -0,0 +1,13 @@
+require("nvchad.mappings")
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<Leader>o", "o<Esc>", { desc = "CMD enter command mode" })
+map("n", "<Leader>O", "O<Esc>", { desc = "CMD enter command mode" })
